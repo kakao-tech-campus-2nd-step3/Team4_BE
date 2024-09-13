9d5cf7c473179bfccc8d48c021935bc68201ec9f
GamJaDo <kim0615157@gmail.com>
2024-09-14 08:34:25 +0900

    Initial commit

---
+++ src/main/java/linkfit/LinkfitApplication.java
@@ -0,0 +1,13 @@
+package linkfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/linkfit/LinkfitApplicationTests.java
@@ -0,0 +1,13 @@
+package linkfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
