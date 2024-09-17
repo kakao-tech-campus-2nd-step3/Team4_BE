272131e838de9ac94b6388704d5b42f2f3ce5feb
GamJaDo <kim0615157@gmail.com>
2024-09-18 00:55:07 +0900

    init

---
+++ src/main/java/linkfit/Team4BeApplication.java
@@ -0,0 +1,13 @@
+package linkfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4BeApplication.class, args);
+	}
+
+}
