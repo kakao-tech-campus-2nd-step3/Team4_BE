28b537fc3c8f4a2d8dbc9b1eb3e9559f5448fbdd
noojmogstel <goaudgo@naver.com>
2024-09-18 21:11:30 +0900

    feat(service): add Career Logic in Trainer Service

--- src/main/java/linkfit/service/TrainerService.java
+++ src/main/java/linkfit/service/TrainerService.java
@@ -1,5 +1,7 @@
 package linkfit.service;
 
+import java.util.List;
+import linkfit.dto.CareerResponse;
 import linkfit.dto.TrainerProfileResponse;
 import linkfit.entity.Trainer;
 import linkfit.repository.TrainerRepository;
@@ -11,15 +13,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class TrainerService {
 
     private final TrainerRepository trainerRepository;
+    private final CareerService careerService;
 
-    public TrainerService(TrainerRepository trainerRepository) {
+    public TrainerService(TrainerRepository trainerRepository, CareerService careerService) {
         this.trainerRepository = trainerRepository;
+        this.careerService = careerService;
     }
 
+    //Trainer Profile 조회
     public TrainerProfileResponse getProfile(Long trainerId) {
         Trainer trainer = trainerRepository.findById(trainerId)
             .orElseThrow(() -> new IllegalStateException("존재하지 않는 트레이너."));
         return new TrainerProfileResponse(trainer);
+    }
+
+    //Trainer Career 삭제
+    public void deleteCareer(Long careerId) {
+        careerService.deleteCareer(careerId);
+    }
+
+    //Trainer Career 등록
+    public void addCareer(Trainer trainer, CareerResponse res) {
+        careerService.addCareer(trainer, res);
+    }
+
+    //Trainer Career 조회
+    public List<CareerResponse> getCareers(Long trainerId) {
+        return careerService.getAllTrainerCareers(trainerId);
     }
 
 
