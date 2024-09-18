dccd7b7dfecbc316b6d673d5f93eb6736d69ec16
noojmogstel <goaudgo@naver.com>
2024-09-18 23:25:56 +0900

    feat(controller): add Trainer Controller

---
+++ src/main/java/linkfit/controller/TrainerController.java
@@ -0,0 +1,21 @@
+package linkfit.controller;
+
+
+import linkfit.service.TrainerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/trainer")
+public class TrainerController {
+
+    private final TrainerService trainerService;
+
+    public TrainerController(TrainerService trainerService) {
+        this.trainerService = trainerService;
+    }
+
+
+}
