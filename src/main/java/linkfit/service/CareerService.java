258c4cee4b65f746395d60a6064c928fe79ff889
noojmogstel <goaudgo@naver.com>
2024-09-18 20:25:50 +0900

    feat(service): add Career Service

---
+++ src/main/java/linkfit/service/CareerService.java
@@ -0,0 +1,36 @@
+package linkfit.service;
+
+import linkfit.dto.CareerResponse;
+import linkfit.entity.Career;
+import linkfit.entity.Trainer;
+import linkfit.repository.CareerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CareerService {
+
+    private final CareerRepository careerRepository;
+
+    public CareerService(CareerRepository careerRepository) {
+        this.careerRepository = careerRepository;
+    }
+
+
+    public List<CareerResponse> getAllTrainerCareers(Long trainerId) {
+        List<Career> careers = careerRepository.findAllByTrainerId(trainerId);
+        return careers.stream().map(CareerResponse::new).toList();
+    }
+
+    public void addCareer(Trainer trainer, CareerResponse res) {
+        Career career = new Career(trainer, res.getCareer());
+    }
+
+    public void deleteCareer(Long careerId) {
+        Career career = careerRepository.findById(careerId)
+            .orElseThrow(() -> new IllegalStateException("존재하지 않는 경력"));
+
+        careerRepository.deleteById(careerId);
+    }
+}
