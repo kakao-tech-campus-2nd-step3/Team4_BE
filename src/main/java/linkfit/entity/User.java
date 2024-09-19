e5804e513a2ddb94a8b59a3795d65d0458013d62
chris0825 <chris00825@naver.com>
2024-09-19 14:26:21 +0900

    feat: add User entity

---
+++ src/main/java/linkfit/entity/User.java
@@ -0,0 +1,30 @@
+package linkfit.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String local;
+
+    private String profileImageUrl;
+
+    private String goal;
+}
