1cf0272902d1ba6bcaf6e498b54066524a446add
gomj <goaudgo@kangwon.ac.kr>
2024-09-20 13:43:59 +0900

    chore: change Table name(User->Users)
    기존 User 테이블이름이 예약어와 같아 이슈가 발생해 Table 이름을 Users로 변경.

--- src/main/java/linkfit/entity/User.java
+++ src/main/java/linkfit/entity/User.java
@@ -1,13 +1,10 @@
 package linkfit.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import linkfit.dto.UserRequest;
 
 @Entity
+@Table(name="users")
 public class User {
 
     @Id
