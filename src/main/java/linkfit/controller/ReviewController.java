7cb17ba84ba1a304dec8d64cfe5055f69da54e69
noojmogstel <goaudgo@naver.com>
2024-10-03 22:35:08 +0900

    feat: add Review post Permission logic

--- src/main/java/linkfit/controller/ReviewController.java
+++ src/main/java/linkfit/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package linkfit.controller;
 
 import java.util.List;
+import linkfit.dto.ReviewRequest;
 import linkfit.dto.ReviewResponse;
 import linkfit.service.ReviewService;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +45,14 @@ public class ReviewController {
         List<ReviewResponse> list = reviewService.getMyReviewsByTrainer(authorization);
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
+
+    @PostMapping("/{trainerId}")
+    public ResponseEntity<Void> addReview(@RequestHeader("Authorization") String authorization, @RequestBody
+        ReviewRequest request, @PathVariable("trainerId") Long trainerId){
+        reviewService.addReview(authorization,request,trainerId) ;
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 
     @DeleteMapping("/{reviewId}")
     public ResponseEntity<Void> deleteReviewById(
