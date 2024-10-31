adbf6bb87ed06fe9582f118c59d5d8ae3f70448b
chris0825 <chris00825@naver.com>
2024-10-31 15:40:25 +0900

    refactor: change chatting response dto field name from Id to id

--- src/main/java/linkfit/dto/ChattingRoomResponse.java
+++ src/main/java/linkfit/dto/ChattingRoomResponse.java
@@ -1,5 +1,5 @@
 package linkfit.dto;
 
-public record ChattingRoomResponse (Long Id, Long memberId, String memberProfileImage){
+public record ChattingRoomResponse (Long id, Long memberId, String memberProfileImage){
 
 }
