a8e1bcf1505458b0c34eaf073e38632b1a8802c3
noojmogstel <goaudgo@naver.com>
2024-10-31 18:26:15 +0900

    feature: add Pt ongoing User Dummy Data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,5 +28,18 @@ VALUES ('PT', 'FALSE');
 INSERT INTO SPORTS_TB(name, deleted)
 VALUES ('PT', 'FALSE');
 
-INSERT INTO CHATTING_ROOM_TB(user_id,trainer_id)
-VALUES (1,1);
+INSERT INTO CHATTING_ROOM_TB(user_id, trainer_id)
+VALUES (1, 1);
+
+/* 진행중인 PT가 있는 더미데이터 추가*/
+INSERT INTO USER_TB(email, password, name, profile_image_url, location)
+VALUES ('b@naver.com', '$2a$10$/kIrh1IIxo60.jHaISACEOpyrb6/2KpgQYcfHlOJBDuFdl9KrOrBm', 'PT회원 1',
+        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg',
+        '강원특별자치도 춘천시 백령로 1'),
+       ('c@naver.com', '$2a$10$/kIrh1IIxo60.jHaISACEOpyrb6/2KpgQYcfHlOJBDuFdl9KrOrBm', 'PT회원 1',
+        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg',
+        '강원특별자치도 춘천시 백령로 1');
+
+INSERT INTO PT_TB(user_id, trainer_id, total_count, price, start_date, status)
+VALUES (2, 1, 10, 300000, NOW(), 'APPROVAL')
+     , (3, 1, 20, 400000, NOW(), 'APPROVAL');
