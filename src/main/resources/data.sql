179e2a25a68981b9e252b0f8b63daec26c3cb9b1
noojmogstel <goaudgo@naver.com>
2024-11-01 10:07:39 +0900

    fix: modify dummy data profile image

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,18 +7,18 @@ VALUES ('강원특별자치도 춘천시 강원대학길 1', '백령스포츠센
 
 INSERT INTO USER_TB(email, password, name, profile_image_url, location)
 VALUES ('a@naver.com', '$2a$10$/kIrh1IIxo60.jHaISACEOpyrb6/2KpgQYcfHlOJBDuFdl9KrOrBm', '일반회원1',
-        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg',
+        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.png',
         '강원특별자치도 춘천시 백령로 1');
 
 INSERT INTO TRAINER_TB(email, password, name, profile_image_url, gym_id, gender)
 VALUES ('t@naver.com', '$2a$10$/kIrh1IIxo60.jHaISACEOpyrb6/2KpgQYcfHlOJBDuFdl9KrOrBm', '트레이너1',
-        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg', 1, 'MALE');
+        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.png', 1, 'MALE');
 
 INSERT INTO GYM_ADMIN_RELATION_TB(gym_id, trainer_id)
 VALUES (1, 1);
 
 INSERT INTO BODY_INFO_TB(user_id, inbody_image_url, create_date, deleted)
-VALUES (1, 'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg', NOW(),
+VALUES (1, 'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.png', NOW(),
         FALSE);
 
 INSERT INTO SPORTS_TB(name, deleted)
@@ -34,10 +34,10 @@ VALUES (1, 1);
 /* 진행중인 PT가 있는 더미데이터 추가*/
 INSERT INTO USER_TB(email, password, name, profile_image_url, location)
 VALUES ('b@naver.com', '$2a$10$/kIrh1IIxo60.jHaISACEOpyrb6/2KpgQYcfHlOJBDuFdl9KrOrBm', 'PT회원 1',
-        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg',
+        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.png',
         '강원특별자치도 춘천시 백령로 1'),
        ('c@naver.com', '$2a$10$/kIrh1IIxo60.jHaISACEOpyrb6/2KpgQYcfHlOJBDuFdl9KrOrBm', 'PT회원 1',
-        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg',
+        'https://nurspace-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.png',
         '강원특별자치도 춘천시 백령로 1');
 
 INSERT INTO PT_TB(user_id, trainer_id, total_count, price, start_date, status)
