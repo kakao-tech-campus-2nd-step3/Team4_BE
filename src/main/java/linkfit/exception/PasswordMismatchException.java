3d8569566dc5494ef68a8a633bcb9aafc9017b93
Seunghyun Ko <chris00825@naver.com>
2024-11-02 17:46:29 +0900

    chore: delete unused custom exception

--- src/main/java/linkfit/exception/PasswordMismatchException.java
+++
@@ -1,8 +0,0 @@
-package linkfit.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-}
