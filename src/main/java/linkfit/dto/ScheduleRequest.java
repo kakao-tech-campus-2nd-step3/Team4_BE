74d72fdf90ae00bb79c24d155aa6c069117cce02
Seunghyun Ko <chris00825@naver.com>
2024-11-07 12:24:09 +0900

    fix: delete valid pattern

--- src/main/java/linkfit/dto/ScheduleRequest.java
+++ src/main/java/linkfit/dto/ScheduleRequest.java
@@ -1,14 +1,11 @@
 package linkfit.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import linkfit.entity.Pt;
 import linkfit.entity.Schedule;
 
-public record ScheduleRequest(@NotNull
-                              @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-                              LocalDateTime startTime) {
+public record ScheduleRequest(@NotNull LocalDateTime startTime) {
 
     public Schedule toEntity(Pt pt) {
         return new Schedule(pt, startTime);
